c0b5e9da58efe2092d4bf877152040275f125178
dhilipsiva <dhilipsiva@gmail.com>
2017-02-07 16:10:11 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pwdman',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
